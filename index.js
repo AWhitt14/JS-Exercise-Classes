74ed2f66c0f83c11795943c08ca0c049e593722f
Alex Whitt <whitta14@gmail.com>
2020-05-14 19:15:58 -0400

    tak 4 complete

--- index.js
+++ index.js
@@ -112,7 +112,13 @@ const myCar = new Car('Sentra', 60);
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
+  constructor(lamAtt){
+    this.name = lamAtt.Name;
+    this.age = lamAtt.Age;
+    this.location = lamAtt.Location;
+  }
 
+  speak(){return `Hello my name is ${this.name}, I am from ${this.location}`};
 }
 
 /*
@@ -129,8 +135,19 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
-
+class Instructor extends Lambdasian{
+  constructor (inAtt){
+    super(inAtt);
+    this.specialty = inAtt.specialty;
+    this.favLanguage = inAtt.favLanguage;
+    this.catchPhrase = inAtt.catchPhrase;
+  }
+  demo(subject) {
+    return `Today we are learning about ${subject}`;
+  }
+  grade(student,subject){
+    return `${student.name} receives a perfect score on ${subject}`;
+  }
 }
 
 /*
